0a2070889977eda7aa6bf89e577a3cd3ea620044
ndmokoe022 <ndmokoe022@student.wethinkcode.co.za>
2024-02-27 01:17:35 +0200

    first assignment completed

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name? \n")
+age = int(input("Enter your age? \n"))
+location = input("Enter your location? \n")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
